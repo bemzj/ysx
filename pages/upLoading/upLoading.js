64c5ba376aeb45cebe2baf37e08114d67c86dce2
zhongRui_HE <418351897@qq.com>
2018-05-09 11:43:50 +0800

    2018-05-09 11.43

---
+++ pages/upLoading/upLoading.js
@@ -0,0 +1,72 @@
+// pages/upLoading/upLoading.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    detailData:{
+      name:'张小包',
+      company:'公司名称',
+      phone:'联系电话',
+      email:'邮箱',
+      address:'公司地址'
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
